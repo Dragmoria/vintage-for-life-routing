3384ce7c5bce203ce15f5c077edf38caa100fd62
Jarne Kompier <j.kompier@hotmail.nl>
2024-04-17 23:46:38 +0200

    Started on security

--- service/src/main/java/com/vintageforlife/service/repository/UserRepository.java
+++ service/src/main/java/com/vintageforlife/service/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.vintageforlife.service.repository;
 import com.vintageforlife.service.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByEmail(String email);
 }
