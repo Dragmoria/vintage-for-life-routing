30229a45d22d307b0fb9d22f1b4d7b4c58468544
Jarne Kompier <jko@wigersma-sikkema.com>
2024-04-18 16:26:43 +0200

    Working out better controllers
    - Organised controller routes
    - Removed first try on security

--- service/src/main/java/com/vintageforlife/service/service/UserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.vintageforlife.service.service;
-
-import com.vintageforlife.service.exception.EmailNotFoundException;
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserDetailsService {
-    UserDetails loadUserByEmail(String email) throws EmailNotFoundException;
-}
